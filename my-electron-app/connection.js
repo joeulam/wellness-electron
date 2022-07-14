264d4382946c750c5ee191e2dfc1757ac566c015
Joey Lam <joeulam345@gmail.com>
2022-07-14 14:34:29 -0400

    1.0.5

--- my-electron-app/connection.js
+++ my-electron-app/connection.js
@@ -1,7 +1,7 @@
 //Backend stuff dw abt it
 var login = document.getElementById("login");
 login.addEventListener("click",loging);
-import {logining} from "./login.js";
+import {logining} from "./index.js";
 function loging(){
   alert("yas");
   document.getElementById("login").style.color = "blue";
--- my-electron-app/index.js
+++ my-electron-app/index.js
@@ -11,6 +11,23 @@ const createWindow = () => {
     createWindow()
   })
 
+   function logining(){
+    var mysql = require('mysql2');
+     var con = mysql.createConnection({
+       host: "localhost",
+       user: "yourusername",
+       password: "yourpassword"
+     });
+     con.connect(function(err) {
+       if (err){
+         console.log("failed!");
+         throw err
+       }
+       else{
+         console.log("connected!");
+       }
+     });
+   }
 
   
 
