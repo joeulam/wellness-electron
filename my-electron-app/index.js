3bef251fe3486646c0b4c20bdf67f08fa38fb4c7
joeulam <joeulam345@gmail.com>
2022-07-14 20:47:25 -0400

    1.0.8 postgre

--- my-electron-app/index.js
+++ my-electron-app/index.js
@@ -14,21 +14,6 @@ const createWindow = () => {
   
 //THIS FILE IS A SERVER SIDED FILE??//
    
-    var mysql = require('mysql2');
-     var con = mysql.createConnection({
-       host: "localhost",
-       user: "yourusername",
-       password: "yourpassword"
-     });
-     con.connect(function(err) {
-       if (err){
-         console.log("failed!");
-         throw err
-       }
-       else{
-         console.log("connected!");
-       }
-     });
 
   
 
