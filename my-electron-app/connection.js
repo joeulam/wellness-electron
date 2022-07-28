331e60d921bd3238f8a3610c68008945b0a28545
joeulam <joeulam345@gmail.com>
2022-07-28 17:21:48 -0400

    v1.1.5b (Password show now working)

--- my-electron-app/connection.js
+++ my-electron-app/connection.js
@@ -8,6 +8,14 @@ if(document.getElementById("loginbutton")){
   login.addEventListener("click",loging);
 }
 
+function show(){
+  var x = document.getElementById("PassInput");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
 
 function logins(user,pass){
   var pg = require('pg');
@@ -56,29 +64,27 @@ function dashboard()
 
 if(document.getElementById("Submitresp")){
   var subresp = document.getElementById("Submitresp");
+  
   subresp.addEventListener("click",moodradio);
+
 }
   
-
+var mood;
   function moodradio() {
     console.log("running");
-    var ele = document.getElementsByName("moods");
-    var mood = 0;
+    var diarys = document.getElementById("Diary").value;
+    
     var sql;
-    for(i = 0; i < ele.length; i++) {
-        if(ele[i].checked == "bad")
-        {
-          mood = 0;
-  
-        }
-        else if(ele[i].checked == "meh"){
-          mood = 5;
-        }
-        else if(ele[i].checked == "Pretty good")
-        {
-          mood = 10;
-        }
-    }
+      if(document.getElementById("bad").checked){
+        mood = 0;
+      }
+      else if(document.getElementById("meh").checked){
+        mood = 5;
+      }
+      else if(document.getElementById("Pretty good").checked){
+        mood = 10;
+      }
+    
     var pg = require('pg');
     let usernameg = localStorage.getItem("userg");
     let conp = localStorage.getItem("passg");
@@ -88,13 +94,15 @@ if(document.getElementById("Submitresp")){
             console.log(err);
             console.log(usernameg);
             console.log(conp);
+            console.log(diarys);
           }
           else{
-            sql = "INSERT INTO public."+ usernameg +"(moodrating) VALUES ("+mood+")";
+            sql = "INSERT INTO public."+ usernameg +"(moodrating, diary) VALUES ("+mood+", '"+diarys+"')";
             document.getElementById("Submitresp").style.color = "blue";
+            console.log(mood);
             gconn.query(sql, function (err, result) {
               if (err) throw err;
-              console.log("1 record inserted");
+              console.log("2 record inserted");
             });
           }
           
