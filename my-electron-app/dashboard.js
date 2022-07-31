c9c68fbb78900128baa2434d206906b157a09eea
Joey Lam <joeulam345@gmail.com>
2022-07-31 14:20:56 -0400

    Bugged update

--- my-electron-app/dashboard.js
+++ my-electron-app/dashboard.js
@@ -11,3 +11,21 @@ function moodtr()
 {
   window.location = "mood.html";
 }
+
+var sidebaro = document.getElementById("heyo");
+sidebaro.addEventListener("click",side);
+
+function side()
+{
+  sidebar();
+}
+
+function sidebar()
+{
+  document.getElementsByClassName("choices").style.display = "none";
+}
+
+function sidebarclose()
+{
+
+}
