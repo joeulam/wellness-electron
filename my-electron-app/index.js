4319b4296a13bfbd626fe12573239d17e79456fb
Joey Lam <joeulam345@gmail.com>
2022-08-01 23:49:17 -0400

    Minor update

--- my-electron-app/index.js
+++ my-electron-app/index.js
@@ -4,6 +4,7 @@ const createWindow = () => {
     const win = new BrowserWindow({
       width: 1920,
       height: 1080,
+      autoHideMenuBar: true,
       webPreferences: {
         nodeIntegration: true,
         contextIsolation: false,
