045b907b2a8a29b23aad621d41fae24bd430bb26
joeulam <joeulam345@gmail.com>
2022-08-12 14:21:02 -0400

    v1.1.9 (Bare bone app)

---
+++ my-electron-app/task.js
@@ -0,0 +1,52 @@
+var sidebaro = document.getElementById("heyo");
+var sidebaros = document.getElementById("heyobg");
+sidebaros.addEventListener("click",sidebar);
+sidebaro.addEventListener("click",sidebar);
+
+
+var a = 0;
+function sidebar()
+{
+  if(a%2==0)
+  {
+    a++;
+    var elems = document.getElementsByClassName('choices');
+    for (var i=0;i<elems.length;i+=1){
+    elems[i].style.display = 'block';
+    document.getElementsByClassName('sidebar')[i].style.background = "#B07D62";
+    }
+  }
+  else{
+    sidebarclose()
+    a++;
+  }
+
+}
+
+
+function sidebarclose()
+{
+  var elems = document.getElementsByClassName('choices');
+  for (var i=0;i<elems.length;i+=1){
+  elems[i].style.display = 'none';
+  document.getElementsByClassName('sidebar')[i].style.background = "#EBD4AB";
+
+  }
+}
+
+
+
+var sdash = document.getElementById("dash")
+sdash.addEventListener("click",dash)
+function dash(){
+  window.location = "dashboard.html";
+
+}
+
+var stodo = document.getElementById("todo")
+stodo.addEventListener("click",todo)
+function todo(){
+  window.location = "task.html";
+
+}
+
