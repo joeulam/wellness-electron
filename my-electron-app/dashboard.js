cf063ef90b883260220ee456ff3298a04180560c
Joey Lam <joeulam345@gmail.com>
2022-08-21 14:21:22 -0400

    v1.2.8b

--- my-electron-app/dashboard.js
+++ my-electron-app/dashboard.js
@@ -90,6 +90,7 @@ function todo(){
 
 }
 
+//functions//
 function responseget()
         {
           var pg = require('pg');
--- my-electron-app/index.js
+++ my-electron-app/index.js
@@ -20,6 +20,8 @@ const createWindow = () => {
     createWindow()
   })
 
+  
+
 
 
  
